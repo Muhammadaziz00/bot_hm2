e9897188f6b127c9dc4ff824e53a5d754143a83f
Muhammadaziz00 <m.aziz.s.0727@gmail.com>
2023-03-01 20:57:56 +0600

    sas

--- main.py
+++ main.py
@@ -1,88 +1,145 @@
-from aiogram import Bot, Dispatcher, types, executor
-from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram import Bot, types, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
-from pytube import YouTube
 from dotenv import load_dotenv
-from pytube.exceptions import RegexMatchError
-import os
+from pytube import YouTube
 import logging
+import time
+import os
 
-load_dotenv('.env')
+from keyboards import button, share_button
+from start_db import CustomDB
+from custom_states import DownloadAudio, DownloadVideo, Mail
 
-buttons = [
-    KeyboardButton('/start'),
-    KeyboardButton('/help'),
-    KeyboardButton('/video'),
-    KeyboardButton('/audio')
-]
+db = CustomDB()
+connect = db.connect
+db.connect_db()
 
-button = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(*buttons)
+load_dotenv(".env")
 
-bot = Bot(os.environ.get("TOKEN"))
+bot = Bot(os.environ.get('TOKEN'))
 dp = Dispatcher(bot, storage=MemoryStorage())
 storage = MemoryStorage()
 logging.basicConfig(level=logging.INFO)
 
+def url_valid(url):
+    try:
+        YouTube(url).streams.first()
+        return True
+    except:
+        return False
+
 @dp.message_handler(commands=['start'])
 async def start(message:types.Message):
-    await message.answer(f'Здраствуйте {message.from_user.full_name}', reply_markup=button)
+    cursor = connect.cursor()
+    cursor.execute(f"SELECT id FROM users WHERE id = {message.from_user.id};")
+    res = cursor.fetchall()
+    if res == []:
+        cursor.execute(f"INSERT INTO users VALUES ('{message.from_user.username}', '{message.from_user.first_name}', '{message.from_user.last_name}', {message.from_user.id}, '{time.ctime()}');")
+    await message.answer(f'Здравствуйте {message.from_user.full_name}')
+    await message.answer("Этот бот способен скачивать видео или аудио с платформы Youtube.")
+    await message.answer("Если запутаетесь в командах напишите /help", reply_markup=button)
+    connect.commit()
 
-class DownloadVideo(StatesGroup):
-    download = State()
-class DownloadAudio(StatesGroup):
-    downl0ad = State()
+@dp.callback_query_handler(lambda call: call)
+async def all(call):
+    if call.data == "start":
+        await start(call.message)
+    elif call.data == "help":
+        await helper(call.message)
+    elif call.data == "audio":
+        await audio(call.message)
+    elif call.data == "video":
+        await video(call.message)
+
+@dp.message_handler(commands=['help'])
+async def helper(message:types.Message):
+    await message.answer("Вот список команд бота:\n/video - для скачивания видеороликов\n/audio - для скачивания аудио из видеороликов")
+
+@dp.message_handler(commands=['share'])
+async def share_user(message:types.Message):
+    await message.reply(f"{message.from_user.full_name} вы можете поделиться своими данными", reply_markup=share_button)
+
+@dp.message_handler(content_types=types .ContentType.CONTACT)
+async def get_contact(message:types.Message):
+    # print(message.contact)
+
+@dp.message_handler(content_types=types .ContentType.LOCATION)
+async def get_contact(message:types.Message):
+    # print(message.location)
+    cursor = connect.cursor()
+    cursor.execute(f"UPDATE users SET PHONE = '{message.contact['phone_number']}' WHERE id = {message.from_user.id}")
+    await message.answer("ваш контакт записан")
+    
+@dp.message_handler(commands=['audio'])
+async def audio(message:types.Message):
+    await message.reply("Отправьте ссылку на аудио и оно будет скачано.")
+    await DownloadAudio.downloadaud.set()
+
+@dp.message_handler(state=DownloadAudio.downloadaud)
+async def download_audio(message:types.Message, state:FSMContext):
+    if url_valid(message.text) == True:
+        await message.answer("Скачиваем аудио")
+        aud_yt = YouTube(message.text)
+        await message.reply(f'{aud_yt.title}')
+        audio = aud_yt.streams.filter(only_audio=True).first().download('audio', f'{aud_yt.title}.mp3')
+        try:
+            await message.answer("Отправляем аудио...")
+            with open(audio, 'rb') as down_audio:
+                await message.answer_audio(down_audio)
+                os.remove(audio)
+        except:
+            await message.answer("Произошла ошибка при скачивании")
+            os.remove(audio)
+        await state.finish()
+    else:
+            await message.reply("Отправленная вами ссылка недействительна. Отправьте ссылку на Youtube видео.")
 
 @dp.message_handler(commands=['video'])
 async def video(message:types.Message):
-    await message.reply(f'Отправьте ссылку на видео и я вам скачаю его')
+    await message.reply("Отправьте ссылку на видео и оно будет скачано.")
     await DownloadVideo.download.set()
-
-@dp.message_handler(commands=['audio'])
-async def audio(message:types.Message):
-    await message.reply(f'Отправьте ссылку на аудио и я вам скачаю его')
-    await DownloadAudio.downl0ad.set()
-
-@dp.message_handler(state=DownloadAudio.downl0ad)
-async def downaudio(msg:types.Message, state: FSMContext):
-    await msg.answer("download audio")
-    try:
-        yt = YouTube(msg.text)
-    except RegexMatchError:
-        await msg.reply("Неверная ссылка на видео")
-    await msg.reply(f'{yt.title}')
-    audio = yt.streams.filter(only_audio=True).first().download('audio',f"{yt.title}.mp3")
-    try:
-        await msg.answer(f'отправляем аудио')
-        with open(audio, 'rb') as da:
-            await msg.answer_audio(da)
-            os.remove(audio)
-    except:
-        await msg.answer('произошла ошибка при отправке аудио')
-    await state.finish()
 
 @dp.message_handler(state=DownloadVideo.download)
 async def download_video(message:types.Message, state:FSMContext):
-    await message.answer("Скачиваем видео")
-    
-    try:
+    if url_valid( message.text) == True:
+        await message.answer("Скачиваем видео")
         yt = YouTube(message.text)
-    except RegexMatchError:
-        await message.reply("Неверная ссылка на видео")
-    await message.reply(f'{yt.title}')
-    video =  yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download('video', f'{yt.title}.mp4')
-    try:
-        await message.answer(f"Отправляем видео")
-        with open(video, 'rb') as down_video:
-            await message.answer_video(down_video)
+        await message.reply(f'{yt.title}')
+        video = yt.streams.filter(progressive=True, file_extension="mp4").order_by('resolution').desc().first().download('video', f"{yt.title}.mp4")
+        try:
+            await message.answer("Отправляем видео...")
+            with open(video, 'rb') as down_video:
+                await message.answer_video(down_video)
+                os.remove(video)
+        except:
+            await message.answer("Произошла ошибка при скачивании")
             os.remove(video)
-    except:
-        await message.answer("Произошла ошибка при отправке видео")
+    else:
+        await message.reply("Отправленная вами ссылка недействительна. Отправьте ссылку на Youtube видео.")
+        await state.finish()
+
+@dp.message_handler(commands=['mailing'])
+async def mailing(message:types.Message):
+    if message.from_user.id == 1540943180:
+        await message.answer("Введите текст для рассылки: ")
+        await Mail.title.set()
+    else:
+        await message.answer("Нет доступа")
+
+@dp.message_handler(state=Mail.title)
+async def send_mailing(message:types.Message, state:FSMContext):
+    cursor = connect.cursor()
+    cursor.execute(f"SELECT id FROM users;")
+    users_id = cursor.fetchall()
+    await message.answer(f"{users_id}")
+    for id in users_id:
+        await bot.send_message(id[0], message.text)
+    await message.answer("Рассылка окончена")
     await state.finish()
 
 @dp.message_handler()
-async def not_found(message:types.Message):
-    await message.reply(f"Я вас не понял, введите /help")
+async def nothing(message:types.Message):
+    await message.reply("Я вас не понял, введите /help для просмотра доступных функций.")
 
 executor.start_polling(dp)
